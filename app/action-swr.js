7e737d5f40805033e7079147c03e057720444f13
roggc <roggc9@gmail.com>
2024-03-02 07:11:13 +0100

    fix action-swr

--- app/action-swr.js
+++ app/action-swr.js
@@ -4,8 +4,18 @@ import { Suspense, useMemo } from "react";
 import { usePropsChangedKey } from "@/app/hooks";
 import useSWR from "swr";
 
-const callActionAsync = (action, props) =>
+const fetcher = (action, props) =>
   new Promise((r) => setTimeout(async () => r(await action(props))));
+
+const fetcherSWR = ([action, props]) => fetcher(action, props);
+
+const ReadSWR = ({ swrArgs, fetcher }) => {
+  return useSWR(swrArgs, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    suspense: true,
+  }).data;
+};
 
 export default function Action({
   action,
@@ -16,12 +26,10 @@ export default function Action({
 
   const propsForSWR = useMemo(() => props, [propsChangedKey, softKey]);
   const swrArgs = useMemo(() => [action, propsForSWR], [action, propsForSWR]);
-  const fetcher = ([action, props]) => callActionAsync(action, props);
-  const { data } = useSWR(swrArgs, fetcher, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    suspense: true,
-  });
 
-  return <Suspense fallback={children}>{data}</Suspense>;
+  return (
+    <Suspense fallback={children}>
+      <ReadSWR swrArgs={swrArgs} fetcher={fetcherSWR} />
+    </Suspense>
+  );
 }
